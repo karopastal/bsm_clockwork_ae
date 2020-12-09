6782b9698118a6040755843debacf537c2c222a2
karopastal <tpaskaro@gmail.com>
2020-12-09 17:14:55 +0200

    update

--- scripts/parity.sh
+++ scripts/parity.sh
@@ -5,7 +5,9 @@
 export path_to_python="/bin/python3"
 export script_path="/gpfs0/kats/users/talpas/projects/bsm_clockwork_ae/scripts/train_models.py"
 
-COMMANDS = ('src.parity.v8_ae_v8_ds' 'src.parity.parity_ae_parity_ds')
+COMMANDS=('src.parity.v8_ae_v8_ds'
+          'src.parity.parity_ae_parity_ds')
+
 #COMMANDS = ('src.parity.v8_ae_parity_ds' 'src.parity.parity_ae_v8_ds')
 
 for command in "${COMMANDS[@]}"
