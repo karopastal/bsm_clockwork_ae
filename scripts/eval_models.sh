1006402ed7d236d18038bb542b0efd613baac3e2
karopastal <tpaskaro@gmail.com>
2020-12-15 14:03:01 +0200

    update

--- scripts/eval_models.sh
+++ scripts/eval_models.sh
@@ -5,17 +5,18 @@
 export path_to_python="/bin/python3"
 export script_path="/gpfs0/kats/users/talpas/projects/bsm_clockwork_ae/scripts/train_models.py"
 
-MODEL_PATHS=('data/models/conv_kl_ae/conv_kl_ae_2/Dec-14-20_T_16-20-31'
-             'data/models/conv_kl_ae/conv_kl_ae_3/Dec-14-20_T_16-20-31'
-             'data/models/conv_kl_ae/conv_kl_ae_4/Dec-14-20_T_16-20-31'
-             'data/models/conv_kl_ae/conv_kl_ae_5/Dec-14-20_T_16-20-31'
-             'data/models/conv_kl_ae/conv_kl_ae_6/Dec-14-20_T_16-20-31'
-             'data/models/conv_kl_ae_v1/conv_kl_ae_v1_1/Dec-14-20_T_17-27-21'
-             'data/models/conv_kl_ae_v1/conv_kl_ae_v1_3/Dec-14-20_T_17-27-21'
-             'data/models/conv_kl_ae_v1/conv_kl_ae_v1_5/Dec-14-20_T_17-27-21'
-             'data/models/conv_kl_ae_v2/conv_kl_ae_v1_1/Dec-14-20_T_17-27-21'
-             'data/models/conv_kl_ae_v2/conv_kl_ae_v1_3/Dec-14-20_T_17-27-21'
-             'data/models/conv_kl_ae_v2/conv_kl_ae_v1_5/Dec-14-20_T_17-27-21')
+MODEL_PATHS=(
+# 'data/models/conv_kl_ae/conv_kl_ae_2/Dec-14-20_T_16-20-31'
+#             'data/models/conv_kl_ae/conv_kl_ae_3/Dec-14-20_T_16-20-31'
+#             'data/models/conv_kl_ae/conv_kl_ae_4/Dec-14-20_T_16-20-31'
+#             'data/models/conv_kl_ae/conv_kl_ae_5/Dec-14-20_T_16-20-31'
+#             'data/models/conv_kl_ae/conv_kl_ae_6/Dec-14-20_T_16-20-31'
+#             'data/models/conv_kl_ae_v1/conv_kl_ae_v1_1/Dec-14-20_T_17-27-21'
+#             'data/models/conv_kl_ae_v1/conv_kl_ae_v1_3/Dec-14-20_T_17-27-21'
+#             'data/models/conv_kl_ae_v1/conv_kl_ae_v1_5/Dec-14-20_T_17-27-21'
+             'data/models/conv_kl_ae_v2/conv_kl_ae_v2_1/Dec-14-20_T_17-27-21'
+             'data/models/conv_kl_ae_v2/conv_kl_ae_v2_3/Dec-14-20_T_17-27-21'
+             'data/models/conv_kl_ae_v2/conv_kl_ae_v2_5/Dec-14-20_T_17-27-21')
 
 for path in "${MODEL_PATHS[@]}"
 do
