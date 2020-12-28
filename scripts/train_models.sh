a94e648806c1f34bbd4b282b4556ef014145edc9
karopastal <tpaskaro@gmail.com>
2020-12-28 15:49:04 +0200

    create ds for testing

--- scripts/train_models.sh
+++ scripts/train_models.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
 #export path_to_python="/gpfs0/kats/projects/Python-3.8.4/python"
+#qsub -l mem_free=50G,h_vmem=50G -pe shared 10 your_script
 
 export path_to_python="/bin/python3"
 export script_path="/gpfs0/kats/users/talpas/projects/bsm_clockwork_ae/scripts/train_models.py"
 
-MODELS=('conv_kl_ae_v1_1'
-        'conv_kl_ae_v1_2'
-        'conv_kl_ae_v1_3'
-        'conv_kl_ae_v1_4'
-        'conv_kl_ae_v1_5'
-        'conv_kl_ae_v1_6'
-        'conv_kl_ae_v2_1'
-        'conv_kl_ae_v2_2'
+MODELS=('conv_ae_2'
         'conv_kl_ae_v2_3'
-        'conv_kl_ae_v2_4'
-        'conv_kl_ae_v2_5'
-        'conv_kl_ae_v2_6')
+        'conv_kl_ae_4')
 
 
 for model in "${MODELS[@]}"
 do
   echo "Training model: $model"
   export script_path="/gpfs0/kats/users/talpas/projects/bsm_clockwork_ae/scripts/train_models.py $model"
-  qsub -cwd -q kats.q -S $path_to_python $script_path
+  qsub -cwd -q kats.q -l mem_free=50G,h_vmem=50G -pe shared 10 -S $path_to_python $script_path
 done
--- src/dataset.py
+++ src/dataset.py
@@ -113,25 +113,15 @@ def main():
     cwt_generator.create(name='default')
 
     ds = Dataset(cwt_generator,
-                 train_size=25000,
-                 test_bg_size=5000,
-                 test_signal_size=5000)
+                 train_size=10000,
+                 test_bg_size=25000,
+                 test_signal_size=25000)
 
     # ds.build_train_dataset()
-    # ds.build_test_bg_dataset()
-    # ds.build_test_signal_dataset(m_5=6000, k=1000)
-    # ds.build_test_signal_dataset(m_5=6500, k=1000)
-    # ds.build_test_signal_dataset(m_5=7000, k=1000)
-    # ds.build_test_signal_dataset(m_5=7500, k=1000)
-
-    ds.build_test_signal_dataset(m_5=8000, k=1000)
-    ds.build_test_signal_dataset(m_5=8500, k=1000)
-    ds.build_test_signal_dataset(m_5=9000, k=1000)
-    ds.build_test_signal_dataset(m_5=9500, k=1000)
-    ds.build_test_signal_dataset(m_5=10000, k=1000)
-    ds.build_test_signal_dataset(m_5=10500, k=1000)
+    ds.build_test_bg_dataset()
+    ds.build_test_signal_dataset(m_5=6200, k=1000)
 
 
 if __name__ == '__main__':
-    pass
-    # main()
+    # pass
+    main()
