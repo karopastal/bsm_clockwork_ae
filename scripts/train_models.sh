274ae08e33187bd1467ced3899bbf5503c707cea
karopastal <tpaskaro@gmail.com>
2020-12-28 16:45:04 +0200

    update

--- scripts/train_models.sh
+++ scripts/train_models.sh
@@ -3,6 +3,7 @@
 #export path_to_python="/gpfs0/kats/projects/Python-3.8.4/python"
 #qsub -l mem_free=50G,h_vmem=50G -pe shared 10 your_script
 
+
 export path_to_python="/bin/python3"
 export script_path="/gpfs0/kats/users/talpas/projects/bsm_clockwork_ae/scripts/train_models.py"
 
@@ -15,5 +16,5 @@ for model in "${MODELS[@]}"
 do
   echo "Training model: $model"
   export script_path="/gpfs0/kats/users/talpas/projects/bsm_clockwork_ae/scripts/train_models.py $model"
-  qsub -cwd -q kats.q -l mem_free=50G,h_vmem=50G -S $path_to_python $script_path
+  qsub -cwd -q kats.q -S $path_to_python $script_path
 done
